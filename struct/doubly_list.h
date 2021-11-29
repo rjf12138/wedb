8c288dcabe495f61d180aff35edf615abe1dda9c
rjf12138 <3161173206@qq.com>
2021-11-29 22:28:21 +0800

    2021年 11月 29日 星期一 22:28:21 CST

---
+++ struct/doubly_list.h
@@ -0,0 +1,305 @@
+﻿#ifndef __DOUBLY_LIST__
+#define __DOUBLY_LIST__
+#include "basic_header.h"
+
+template<typename V>
+struct DoublyListNode {
+	V value;
+	DoublyListNode<V>* prev, * next;
+
+	DoublyListNode(const V& v, DoublyListNode<V>* p = nullptr, DoublyListNode<V>* n = nullptr)
+		:value(v), prev(p), next(n)
+	{}
+};
+
+template <typename V>
+class DoublyList {
+public:
+	DoublyList(void);
+	virtual ~DoublyList(void);
+
+	class Iterator {
+		friend class DoublyList;
+	public:
+		Iterator(void);
+		~Iterator(void);
+
+		V& operator*();
+		Bool operator==(const Iterator& rhs);
+		Bool operator!=(const Iterator& rhs);
+		Iterator& operator=(const Iterator& rhs);
+
+		Iterator& operator++();
+		Iterator operator++(Int32); // 后置
+
+		Iterator& operator--();
+		Iterator operator--(Int32); // 前置
+
+	private:
+		const DoublyList* list_;
+		DoublyListNode* node_;
+	};
+
+	Iterator begin(void);
+	Iterator end(void);
+
+	Int32 push_back(const V& value);
+	Int32 insert_front(const Iterator& iter, const V& value);
+	Int32 insert_back(const Iterator& iter, const V& value);
+	Int32 erase(const Iterator& iter);
+	Int32 clear(void);
+	UInt32 size(void) const;
+
+private:
+	DoublyListNode<V>* get_node(const Iterator& iter);
+
+private:
+	UInt32 size_;
+	DoublyListNode<V> head_;
+	DoublyListNode<V> tail_;
+};
+
+template <typename V>
+DoublyList<V>::DoublyList(void)
+	:size_(0),
+	head_(nullptr, &tail_),
+	tail_(&head_, nullptr)
+{
+}
+
+template <typename V>
+DoublyList<V>::~DoublyList(void)
+{
+	this->clear();
+}
+
+template <typename V>
+UInt32 DoublyList<V>::size(void) const
+{
+	return size_;
+}
+
+template <typename V>
+Int32 DoublyList<V>::clear(void)
+{
+	DoublyList<V>::Iterator iter = this->begin();
+	while (iter != this->end()) {
+		this->erase(iter);
+		iter = this->begin();
+	}
+
+	return 0;
+}
+
+template <typename V>
+DoublyList<V>::Iterator DoublyList<V>::begin(void)
+{
+	Iterator iter(this);
+	iter.node_ = head_->next;
+
+	return iter;
+}
+
+template <typename V>
+DoublyList<V>::Iterator DoublyList<V>::end(void)
+{
+	Iterator iter(this);
+	iter.node_ = &tail_;
+
+	return iter;
+}
+
+template <typename V>
+DoublyListNode<V>* DoublyList<V>::get_node(const Iterator& iter)
+{
+	if (iter.list_ != this) {
+		return nullptr;
+	}
+
+	return iter.node_;
+}
+
+template <typename V>
+Int32 DoublyList<V>::push_back(const V& value)
+{
+	DoublyListNode<V>* new_node = new DoublyListNode<V>(value, tail_.prev, &tail_);
+	tail_.prev->next = new_node;
+	tail_.prev = new_node;
+	++size_;
+
+	return 0;
+}
+
+template <typename V>
+Int32 DoublyList<V>::insert_front(const Iterator& iter, const V& value)
+{
+	DoublyListNode<V>* curr_node = this->get_node(iter);
+	if (curr_node == nullptr || curr_node == &head_) {
+		throw std::runtime_error("DoublyList<V>::insert_front(): Insert value in the wrong postion.");
+	}
+
+	DoublyListNode<V>* new_node = new DoublyListNode<V>(value, curr_node->prev, curr_node);
+	curr_node->prev->next = new_node;
+	curr_node->prev = new_node;
+	++size_;
+
+	return 0;
+}
+
+template <typename V>
+Int32 DoublyList<V>::insert_back(const Iterator& iter, const V& value)
+{
+	DoublyListNode<V>* curr_node = this->get_node(iter);
+	if (curr_node == nullptr || curr_node == &tail_) {
+		throw std::runtime_error("DoublyList<V>::insert_back(): Insert value in the wrong postion.");
+	}
+
+	DoublyListNode<V>* new_node = new DoublyListNode<V>(value, curr_node, curr_node->next);
+	curr_node->next->prev = new_node;
+	curr_node->next = new_node;
+	++size_;
+
+	return 0;
+}
+
+template <typename V>
+Int32 DoublyList<V>::erase(const Iterator& iter)
+{
+	DoublyListNode<V>* curr_node = this->get_node(iter);
+	if (curr_node == nullptr || curr_node == &head_ || curr_node == &tail_) {
+		throw std::runtime_error("DoublyList<V>::erase(): Erase the value in the wrong postion.");
+	}
+
+	curr_node->prev->next = curr_node->next;
+	curr_node->next->prev = curr_node->prev;
+
+	--size_;
+	delete curr_node;
+
+	return 0;
+}
+
+////////////////////////////////////////////////////
+
+template <typename V>
+DoublyList<V>::Iterator::Iterator(void)
+	:list_(nullptr), node_(nullptr)
+{
+
+}
+
+
+template <typename V>
+DoublyList<V>::Iterator::~Iterator(void)
+{
+
+}
+
+template <typename V>
+V& DoublyList<V>::Iterator::operator*()
+{
+	if (list_ == nullptr || node_ == nullptr) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator*(): Iterator not pointer any DoublyList node.");
+	}
+
+	if (node_ == &list_->head_ || node_ == &list->tail_) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator*(): Iterator not pointer any DoublyList node.");
+	}
+
+	return node_->value;
+}
+
+template <typename V>
+Bool DoublyList<V>::Iterator::operator==(const DoublyList<V>::Iterator& rhs)
+{
+	if (list_ == nullptr || node_ == nullptr) {
+		return False;
+	}
+
+	if (list_ == rhs.list_ && node_ == rhs.node_) {
+		return True;
+	}
+
+	return False;
+}
+
+template <typename V>
+Bool DoublyList<V>::Iterator::operator!=(const DoublyList<V>::Iterator& rhs)
+{
+	return !(*this == rhs);
+}
+
+template <typename V>
+DoublyList<V>::Iterator& DoublyList<V>::Iterator::operator=(const DoublyList<V>::Iterator& rhs)
+{
+	list_ = rhs.list_;
+	node_ = rhs.node_;
+
+	return *this;
+}
+
+template <typename V>
+DoublyList<V>::Iterator& DoublyList<V>::Iterator::operator++()
+{
+	if (list_ == nullptr || node_ == nullptr) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator++(): Iterator not pointer any DoublyList node.");
+	}
+
+	if (node_ == &list_->head_ || node_ == &list->tail_) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator++(): Iterator not pointer any DoublyList node.");
+	}
+
+	node_ = node_->next;
+	return *this;
+}
+
+template <typename V>
+DoublyList<V>::Iterator DoublyList<V>::Iterator::operator++(Int32)
+{
+	if (list_ == nullptr || node_ == nullptr) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator++(Int32): Iterator not pointer any DoublyList node.");
+	}
+
+	if (node_ == &list_->head_ || node_ == &list->tail_) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator++(Int32): Iterator not pointer any DoublyList node.");
+	}
+
+	Iterator tmp = *this;
+	node_ = node_->next;
+
+	return tmp;
+}
+
+template <typename V>
+DoublyList<V>::Iterator& DoublyList<V>::Iterator::operator--()
+{
+	if (list_ == nullptr || node_ == nullptr) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator--(): Iterator not pointer any DoublyList node.");
+	}
+
+	if (node_ == &list_->head_ || node_ == &list->tail_) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator--(): Iterator not pointer any DoublyList node.");
+	}
+
+	node_ = node_->prev;
+	return *this;
+}
+
+template <typename V>
+DoublyList<V>::Iterator DoublyList<V>::Iterator::operator--(Int32)
+{
+	if (list_ == nullptr || node_ == nullptr) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator--(Int32): Iterator not pointer any DoublyList node.");
+	}
+
+	if (node_ == &list_->head_ || node_ == &list->tail_) {
+		throw std::runtime_error("DoublyList<V>::Iterator::operator--(Int32): Iterator not pointer any DoublyList node.");
+	}
+
+	Iterator tmp = *this;
+	node_ = node_->prev;
+
+	return tmp;
+}
+
+#endif
---
+++ struct/skip_list.h
@@ -0,0 +1,214 @@
+﻿#pragma once
+#include "basic_header.h"
+#define MAX_LEVEL   32
+
+template<typename K>
+struct Compare {
+	Int32 operator()(K key1, K key2) {
+		if (key1 > key2) {
+			return 1;
+		}
+		else if (key1 < key2) {
+			return -1;
+		}
+
+		return 0;
+	}
+};
+
+template<typename K, typename V>
+struct SkipListNode {
+	K key;
+	V value;
+	Int8 level;
+	SkipListNode<K, V>* next[MAX_LEVEL];
+};
+
+template<typename K, typename V, typename Comparator = Compare<K>>
+class SkipList {
+public:
+	SkipList(void);
+	virtual ~SkipList(void);
+
+	Int32 get(K key, V& value);
+	Int32 erase(K key);
+
+	Int32 put(K key, V value);
+	Int32 size(void);
+
+private:
+	Int8 random_height(void);
+
+private:
+	Int32 size_;
+	Comparator compare_;
+	SkipListNode<K, V> header_;
+	SkipListNode<K, V> tail_;
+};
+
+template<typename K, typename V, class Comparator>
+SkipList<K, V, Comparator>::SkipList(void)
+	:size_(0)
+{
+	header_.level = MAX_LEVEL;
+	for (Int32 i = 0; i <= header_.level; ++i) {
+		header_.next[i] = &tail_;
+	}
+}
+
+template<typename K, typename V, class Comparator>
+SkipList<K, V, Comparator>::~SkipList(void)
+{
+	SkipListNode<K, V>* next_ptr = header_.next[0];
+	while (next_ptr != &tail_) {
+		SkipListNode<K, V>* remove_ptr = next_ptr;
+		next_ptr = remove_ptr->next[0];
+		delete remove_ptr;
+	}
+}
+
+template<typename K, typename V, class Comparator>
+Int32 SkipList<K, V, Comparator>::get(K key, V& value)
+{
+	Int8 level = header_.level;
+	SkipListNode<K, V>* ptr = &header_;
+	while (level >= 0) {
+		if (ptr->next[level] == &tail_ || compare_(ptr->next[level]->key, key) == 1) {
+			if (level > 0) {
+				--level;
+			}
+			else {
+				break;
+			}
+		}
+		else if (compare_(ptr->next[level]->key, key) == -1) {
+			ptr = ptr->next[level];
+		}
+		else if (compare_(ptr->next[level]->key, key) == 0) {
+			value = ptr->next[level]->value;
+			return 0;
+		}
+	}
+
+	return -1;
+}
+
+template<typename K, typename V, class Comparator>
+Int32 SkipList<K, V, Comparator>::put(K key, V value)
+{
+	Int8 level = header_.level;
+	SkipListNode<K, V>* ptr = &header_;
+	SkipListNode<K, V>* new_node = new SkipListNode<K, V>;
+	new_node->key = key;
+	new_node->value = value;
+	new_node->level = this->random_height();
+
+	while (level >= 0) {
+		if (ptr->next[level] == &tail_ || compare_(ptr->next[level]->key, key) == 1) {
+			if (level <= new_node->level) {
+				new_node->next[level] = ptr;
+			}
+
+			if (level > 0) {
+				--level;
+			}
+			else {
+				break;
+			}
+		}
+		else if (compare_(ptr->next[level]->key, key) == -1) {
+			ptr = ptr->next[level];
+		}
+		else if (compare_(ptr->next[level]->key, key) == 0) {
+			delete new_node;
+			return -1;
+		}
+	}
+
+	for (Int32 i = new_node->level; i >= 0; --i) {
+		SkipListNode<K, V>* tmp_ptr = new_node->next[i];
+		if (tmp_ptr != &tail_) {
+			new_node->next[i] = tmp_ptr->next[i];
+			tmp_ptr->next[i] = new_node;
+		}
+		else {
+			new_node->next[i] = &tail_;
+		}
+	}
+	++size_;
+
+	return 0;
+}
+
+template<typename K, typename V, class Comparator>
+Int32 SkipList<K, V, Comparator>::erase(K key)
+{
+	Int8 level = header_.level;
+	SkipListNode<K, V>* ptr = &header_, * remove_ptr = nullptr;
+	while (level >= 0) {
+		if (ptr->next[level] == &tail_ || compare_(ptr->next[level]->key, key) == 1) {
+			if (level > 0) {
+				--level;
+			}
+			else {
+				break;
+			}
+		}
+		else if (compare_(ptr->next[level]->key, key) == -1) {
+			ptr = ptr->next[level];
+		}
+		else if (compare_(ptr->next[level]->key, key) == 0) {
+			remove_ptr = ptr->next[level];
+			ptr->next[level] = remove_ptr->next[level];
+			--level;
+		}
+	}
+
+	if (remove_ptr != nullptr) {
+		--size_;
+		delete remove_ptr;
+
+		return 0;
+	}
+
+
+	return -1;
+}
+
+template<typename K, typename V, class Comparator>
+Int32 SkipList<K, V, Comparator>::size(void)
+{
+	return size_;
+}
+
+template<typename K, typename V, class Comparator>
+Int8 SkipList<K, V, Comparator>::random_height(void)
+{
+	Int8 height = 0;
+	for (Int32 i = 0; i < MAX_LEVEL; ++i) {
+		if (rand() % 2 == 1) {
+			++height;
+		}
+	}
+
+	return height;
+}
+
+//template<typename K, typename V, class Comparator>
+//void SkipList<K, V,Comparator>::print(void)
+//{
+//    SkipListNode<K, V>* ptr = &header_;
+//
+//    while (ptr->next[0] != &tail_) {
+//        std::cout << "key: " << ptr->next[0]->key << ", value: " << ptr->next[0]->value << std::endl;
+//        ptr = ptr->next[0];
+//    }
+//}
+//
+//template<typename K, typename V, class Comparator>
+//void SkipList<K, V,Comparator>::print_rand()
+//{
+//    for (int i = 0; i < MAX_LEVEL; ++i) {
+//        std::cout << i << ": " << (rand_num[i]) << std::endl;
+//    }
+//}
