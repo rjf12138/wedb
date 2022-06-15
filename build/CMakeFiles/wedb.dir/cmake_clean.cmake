253b8a5c004a1016cab87bf3659c4fca29e7f105
rjf12138 <3161173206@qq.com>
2022-06-16 07:38:51 +0800

    2022年 06月 16日 星期四 07:38:51 CST

---
+++ build/CMakeFiles/wedb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wedb.dir/main/DSN_hash_table_test.cc.o"
+  "CMakeFiles/wedb.dir/main/DSN_hash_table_test.cc.o.d"
+  "wedb"
+  "wedb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wedb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
