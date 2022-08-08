1ccf06042bcee0dd8a9c735faf2e8180b6ff493d
rjf12138 <3161173206@qq.com>
2022-08-08 22:59:27 +0800

    2022年 08月 08日 星期一 22:59:27 CST

--- build/CMakeFiles/wedb.dir/flags.make
+++ build/CMakeFiles/wedb.dir/flags.make
@@ -6,5 +6,5 @@ CXX_DEFINES =
 
 CXX_INCLUDES = -I/home/ruanjian/workspace/project_v/wedb/./inc -I/home/ruanjian/workspace/project_v/wedb/./extern_inc -I/home/ruanjian/workspace/project_v/wedb/struct -I/home/ruanjian/workspace/project_v/wedb/src
 
-CXX_FLAGS = -g -DCHECK_PTHREAD_RETURN_VALUE -D_FILE_OFFSET_BITS=64 -Wall -Wextra -Werror -Wconversion -Wno-unused-parameter -Wold-style-cast -Woverloaded-virtual -Wpointer-arith -Wshadow -Wwrite-strings -march=native -rdynamic
+CXX_FLAGS = -g -DCHECK_PTHREAD_RETURN_VALUE -D_FILE_OFFSET_BITS=64 -Wall -Wextra -Werror -Wconversion -Wno-unused-parameter -Wold-style-cast -Woverloaded-virtual -Wpointer-arith -Wshadow -Wwrite-strings -march=native -rdynamic -g
 
--- struct/dsn_hash_table_v2.h
+++ struct/dsn_hash_table_v2.h
@@ -14,13 +14,13 @@ enum EFSetOp {
     EFSetOp_Remove,
 };
 
+int hash(int key, int value) {
+    return value % key; 
+}
+
 struct FSetValue {
     int value;      // 值
     bool valid;     // 当前值是否有效
-
-    int hash(int key) {
-        return value % key; 
-    }
 
     FSetValue(void)
     :value(0),
@@ -42,12 +42,6 @@ struct FSetOp {
     resp(false),
     prio(0)
     {}
-};
-
-enum EFSetOperate {
-    EFSetOperate_Merge,         // 合并
-    EFSetOperate_FirstSplit,    // 第一次分割
-    EFSetOperate_SecondSplit,   // 将剩余的元素分割
 };
 
 struct FSetNode {
@@ -90,7 +84,17 @@ struct FSetNode {
         return false;
     }
 
+    // 数组改成动态增长的，当前问题如果增长时复制旧值，但是复制完后进行删除操作了
+    // 之后继续进行插入新值，那么已经被删除的值又被恢复了。
+    // 或是继续采用固定长度的数组思考
     bool insert(const FSetValue &val) {
+        if (size_ == max_size_) {
+            FSetValue *tmp_ptr = new FSetValue[max_size_*2];
+            for (int i = 0, j = 0; i < max_size_; ++i) {
+
+            }
+        }
+
         for (int i = 0; i < max_size_; ++i) {
             if (os::Atomic<bool>::compare_and_swap(&values_ptr[i].valid, false, true)) {
                 values_ptr[i].value = val.value;
@@ -108,7 +112,7 @@ struct FSetNode {
     int split(FSetNode &node, int key, int des) {
         int move_size = 0;
         for (int i = 0; i < max_size_; ++i) {
-            if (values_ptr[i].valid == true && values_ptr[i].hash(key) == des) {
+            if (values_ptr[i].valid == true && hash(key, values_ptr[i].value) == des) {
                 if (os::Atomic<bool>::compare_and_swap(&values_ptr[i].valid, true, false) && node.insert(values_ptr[i]) == true) {
                     os::Atomic<int>::fetch_and_sub(&size_, 1);
                     ++move_size;
@@ -219,17 +223,11 @@ public:
     FSetNode node_;
     bool flag_;
 };
-//  TODO
-// HNode 初始化问题 init_bucket会初始化HNode中的某个bucket
-// 但现在两边HNode和DSSet不大相容
+
 class HNode {
 public:
-    HNode(void):
-    size_(0),
-    buckets_ptr_(nullptr),
-    pred_ptr_(nullptr) 
+    HNode(void)
     {
-        resize(16);
     }
 
     ~HNode(void) {
@@ -239,80 +237,66 @@ public:
         }
     }
 
-    int resize(int size) {
-        if (size < size_ || size < 0) {
-            return -1;
+    // 添加功能什么时候扩大哈希表，什么时候缩小哈希表
+
+private:
+    int resize(bool grow) {
+        if (curr_size_ < 2) {
+            return curr_size_;
         }
 
-        if (buckets_ptr_ != nullptr) {
-            delete buckets_ptr_;
-            buckets_ptr_ = nullptr;
+        for (int i = 0; i < curr_size_; ++i) {
+            init_buckets(i);
         }
-        buckets_ptr_ = new FSet*[size];
-        size_ = size;
+
+        for (int i = 0; i < pred_size_; ++i) {
+            auto tmp_bucket_ptr = pred_buckets_ptr_[i];
+            pred_buckets_ptr_[i] = nullptr;
+            if (tmp_bucket_ptr != nullptr) {
+                delete tmp_bucket_ptr;
+            }
+        }
+        delete[] pred_buckets_ptr_;
+        pred_buckets_ptr_ = buckets_ptr_;
+
+        int size = (grow == true ? curr_size_ * 2 : curr_size_ / 2);
+        FSet **tmp_buckets_ptr_ = new FSet*[size];
+        os::Atomic<FSet**>::compare_and_swap(&buckets_ptr_, buckets_ptr_, tmp_buckets_ptr_);
 
         return size;
     }
 
-    // 从node中读取数据到本地
-    int split(FSet &node, int pos, int key, int des) {
-        node.freeze();
-        return node.node_.split(buckets_ptr_[pos]->node_, size_, pos);
-    }
-
-    // 从node中读取数据到本地
-    int merge(FSet &node, int pos) {
-        node.freeze();
-        return node.node_.merge(buckets_ptr_[pos]->node_);
-    }
-
-public:
-    int size_;
-    FSet **buckets_ptr_;
-    HNode *pred_ptr_;
-};
-
-class DSHSet {
-public:
-    DSHSet(void);
-    ~DSHSet(void);
-
-    bool contain(FSetValue val) {
-        FSet *set = node_.buckets_ptr_[val.hash(node_.size_)];
-        if (set->has_member(val) == false) {
-            if (node_.pred_ptr_ != nullptr) {
-                set = node_.pred_ptr_->buckets_ptr_[val.hash(node_.size_)];
-            } 
-        }
-
-        return set->has_member(val);
-    }
-
-    int resize(bool grow) {
-        if (node_.size_ > 1 && grow == true) {
-
-        }
-    }
-
     void init_buckets(int pos) {
-        FSet *set_ptr = node_.buckets_ptr_[pos];
-        HNode *pred_ptr = node_.pred_ptr_;
-        if (set_ptr->node_.size() == 0 && pred_ptr != nullptr) {
+        FSet *bucket_ptr = buckets_ptr_[pos];
+        if (bucket_ptr == nullptr && pred_buckets_ptr_ != nullptr) {
             FSet *new_bucket_ptr = new FSet();
-            if (node_.size_ == pred_ptr->size_ * 2) {
-                node_.split();
+            if (this->curr_size_ == pred_size_ * 2) {
+                FSet *pred_bucket_ptr = pred_buckets_ptr_[hash(pos, pred_size_)];
+                if (pred_bucket_ptr != nullptr) {
+                    pred_bucket_ptr->freeze();
+                    pred_bucket_ptr->node_.split(new_bucket_ptr->node_, pos, bucket_ptr->node_.size());
+                }
             } else {
-                pred_ptr->buckets_ptr_[pos].freeze();
-                pred_ptr->buckets_ptr_[pos].node_.merge(new_bucket_ptr->node_);
+                if (pred_buckets_ptr_[pos] != nullptr) {
+                    pred_buckets_ptr_[pos]->freeze();
+                    pred_buckets_ptr_[pos]->node_.merge(new_bucket_ptr->node_);
+                }
 
-                pred_ptr->buckets_ptr_[pos + pred_ptr->size_].freeze();
-                pred_ptr->buckets_ptr_[pos + pred_ptr->size_].node_.merge(new_bucket_ptr->node_);
+                if (pred_buckets_ptr_[pos + pred_size_] != nullptr) {
+                    pred_buckets_ptr_[pos + pred_size_]->freeze();
+                    pred_buckets_ptr_[pos + pred_size_]->node_.merge(new_bucket_ptr->node_);
+                }
             }
-            os::Atomic<FSet*>::compare_and_swap((node_.buckets_ptr_[pos]), nullptr, new_bucket_ptr);
+            os::Atomic<FSet*>::compare_and_swap(&buckets_ptr_[pos], nullptr, new_bucket_ptr);
         }
     }
+
 private:
-    HNode node_;
+    int curr_size_;
+    FSet **buckets_ptr_;        // 当前的hash数据集
+
+    int pred_size_;
+    FSet **pred_buckets_ptr_;   // 前置的hash数据集
 };
 
 #endif
