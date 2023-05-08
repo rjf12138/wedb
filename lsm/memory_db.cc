a1c05aa41a3d8dc274f97e369b053f1d42f3332a
rjf12138 <3161173206@qq.com>
2023-05-08 22:16:24 +0800

    2023年 05月 08日 星期一 22:16:24 CST

--- lsm/memory_db.cc
+++ lsm/memory_db.cc
@@ -25,30 +25,17 @@ KeyValueBlock::put(const std::string &key, const basic::ByteBuffer& value)
 {
     // 检查关键字和值得长度
     if (key.length() > MAX_TWO_BYTE_LENGTH || value.data_size() > MAX_THREE_BYTE_LENGTH) {
-        err_handle_.set_err_code(ERROR_Code_KeyOrValue_Length_TooLong);
+        this->set_err_code(ERROR_Code_KeyOrValue_Length_TooLong);
         return false;
     }
+
+    this->reset();
 
     // 对关键字和值进行赋值
     key_ = key;
     value_ = value;
 
-    // 设置长度相关的标志位
-    if (key_.length() <= MAX_ONE_BYTE_LENGTH) {
-        this->set_mark(EKeyValueMarkType_ExtendKeyLength, false);
-    } else {
-        this->set_mark(EKeyValueMarkType_ExtendKeyLength, true);
-    }
-
-    if (value_.data_size() <= MAX_ONE_BYTE_LENGTH) {
-        this->set_mark(EKeyValueMarkType_ExtendValueLength, false, EValueLengthExtendType_1bit);
-    } else if (value_.data_size() <= MAX_TWO_BYTE_LENGTH) {
-        this->set_mark(EKeyValueMarkType_ExtendValueLength, false, EValueLengthExtendType_2bit);
-    } else {
-        this->set_mark(EKeyValueMarkType_ExtendValueLength, false, EValueLengthExtendType_3bit);
-    }
-
-    err_handle_.set_err_code(ERROR_OK);
+    this->set_err_code(ERROR_OK);
     return 0;
 }
 
@@ -65,23 +52,6 @@ KeyValueBlock::value(void)
 }
 
 void 
-KeyValueBlock::set_mark(EKeyValueMarkType type, bool value, EValueLengthExtendType value_len_extend_type)
-{
-    switch (type)
-    {
-    case EKeyValueMarkType_Extend:
-    case EKeyValueMarkType_ExtendKeyLength:
-    case EKeyValueMarkType_ExtendValueLength:
-    case EKeyValueMarkType_Compress:
-    case EKeyValueMarkType_Check:
-    case EKeyValueMarkType_Operation:
-    case EKeyValueMarkType_Reserved:
-    default:
-        break;
-    }
-}
-
-void 
 KeyValueBlock::reset(void)
 {
     mark_ = 0x00;
@@ -90,14 +60,88 @@ KeyValueBlock::reset(void)
 }
 
 basic::ByteBuffer 
-KeyValueBlock::to_block(void)
+KeyValueBlock::to_stream(void)
 {
+    // 流格式： | mark | 扩展内容长度（可选） | 扩展内容(可选) | key_length | key | value_length  | value | 校验 （默认检查）|
+    basic::ByteBuffer buffer;
 
+    // 写入crc校验
+    if (this->get_bit(EKeyValueMarkType_Check, 1) != 0) {
+        buffer.write_bytes(&crc16_, 2);
+    }
+
+    // 将值写入到流中
+    buffer += value_;
+    uint32_t value_size = value_.data_size();
+    if (value_.data_size() <= MAX_ONE_BYTE_LENGTH) {
+        this->set_bit(EKeyValueMarkType_ExtendValueLength, EValueLengthExtendType_1bit, 2);
+        buffer.write_bytes(&value_size, 1); // 用1个字节表示值长度
+    } else if (value_.data_size() <= MAX_TWO_BYTE_LENGTH) {
+        this->set_bit(EKeyValueMarkType_ExtendValueLength, EValueLengthExtendType_2bit, 2);
+        buffer.write_bytes(&value_size, 2); // 用2个字节表示值长度
+    } else {
+        this->set_bit(EKeyValueMarkType_ExtendValueLength, EValueLengthExtendType_3bit, 2);
+        buffer.write_bytes(&value_size, 3); // 用3个字节表示值长度
+    }
+
+    // 将关键字写入到流中
+    buffer += key_;
+    uint16_t key_size = key_.length();
+    if (key_.length() <= MAX_ONE_BYTE_LENGTH) {
+        this->set_bit(EKeyValueMarkType_ExtendKeyLength, 0, 1);
+        buffer.write_bytes(&key_size, 1);   // 用1个字节表示值长度
+    } else {
+        this->set_bit(EKeyValueMarkType_ExtendKeyLength, 1, 1);
+        buffer.write_bytes(&key_size, 2);   // 用2个字节表示值长度
+    }
+
+    // 是否写入扩展内容
+    if (this->get_bit(EKeyValueMarkType_Extend, 1) != 0) {
+        buffer += extend_data_;
+
+        auto extend_data_size = extend_data_.data_size();
+        buffer.write_bytes(&extend_data_size, 1);
+    }
+
+    // 写入mark标志字节
+    buffer.write_int8(mark_);
 }
 
 bool 
-KeyValueBlock::from_block(const basic::ByteBuffer &block)
+KeyValueBlock::from_stream(const basic::ByteBuffer &block)
 {
-
+    //TODO
 }
-    
+
+void 
+KeyValueBlock::set_bit(uint8_t pos, uint8_t value, uint8_t bit_len)
+{
+    if (bit_len > 8 || bit_len <= 0) {
+        return ;
+    }
+
+    // 创建一个bit_len长度的掩码 mask，用于获取 value bit_len长度的比特位
+    // 然后设到mark_对应的位置中
+    uint8_t mask = 0x00;
+    for (auto i = 0; i < bit_len; ++i) {
+        mask |= (0x01 << i);
+    }
+
+    mark_ |= (mask & value) << pos;
+}
+
+uint8_t 
+KeyValueBlock::get_bit(uint8_t pos, uint8_t bit_len)
+{
+    if (bit_len > 8 || bit_len <= 0) {
+        return ;
+    }
+
+    // 创建一个bit_len长度的掩码 mask，用于获取 pos 位置的比特位的值
+    uint8_t mask = 0x00;
+    for (auto i = 0; i < bit_len; ++i) {
+        mask |= (0x01 << i);
+    }
+
+    return (mark_ & (mask << pos)) >> pos;
+}
--- lsm/memory_db.h
+++ lsm/memory_db.h
@@ -15,7 +15,7 @@ enum EDBOperationType {
 //1. 扩展位：用于扩展mark字节作用，扩展内容不应超过一个字节所能表达的长度范围，扩展内容设定和解析自定义
 //2. key长度扩展: 默认key的长度最大不能超过一个字节所能表示的范围，设置该位后，key的长度范围是两个字节能表示的范围
 //3. 
-//4. value长度扩展: 3和4表示值得长度，00：一个字节表示长度，01：两个字节表示长度，10：3个字节表示长度。
+//4. value长度扩展: 3和4表示值得长度，01：一个字节表示长度，10：两个字节表示长度，11：3个字节表示长度。
 //	（注：虽然值的最大长度能用3个字节表示，不过单个value的最大不能超过16MB。234位会根据内容长度自动设置）
 //5. 压缩位：该位设置后会对value进行压缩
 //6. 校验位：该位设置后，会对关键字和值进行校验检查内容是否正确（CRC校验）
@@ -23,19 +23,19 @@ enum EDBOperationType {
 //8. 保留
 
 enum EKeyValueMarkType {
-    EKeyValueMarkType_Extend = 0,
-    EKeyValueMarkType_ExtendKeyLength = 1,
-    EKeyValueMarkType_ExtendValueLength = 2,
-    EKeyValueMarkType_Compress = 4,
-    EKeyValueMarkType_Check = 5,
-    EKeyValueMarkType_Operation = 6,
-    EKeyValueMarkType_Reserved = 7,
+    EKeyValueMarkType_Extend = 1,
+    EKeyValueMarkType_ExtendKeyLength = 2,
+    EKeyValueMarkType_ExtendValueLength = 3,
+    EKeyValueMarkType_Compress = 5,
+    EKeyValueMarkType_Check = 6,
+    EKeyValueMarkType_Operation = 7,
+    EKeyValueMarkType_Reserved = 8,
 };
 
 enum EValueLengthExtendType {
-    EValueLengthExtendType_1bit,
-    EValueLengthExtendType_2bit,
-    EValueLengthExtendType_3bit,
+    EValueLengthExtendType_1bit = 1,
+    EValueLengthExtendType_2bit = 2,
+    EValueLengthExtendType_3bit = 3,
 };
 
 #define MAX_ONE_BYTE_LENGTH     0xFF
@@ -43,7 +43,7 @@ enum EValueLengthExtendType {
 #define MAX_THREE_BYTE_LENGTH   0xFFFFFF
 
 
-class KeyValueBlock {
+class KeyValueBlock : public basic::ErrHandle {
 public:
     KeyValueBlock(void);
     KeyValueBlock(const std::string &key, const basic::ByteBuffer& value);
@@ -56,28 +56,34 @@ public:
     std::string &key(void);
     // 获取值
     basic::ByteBuffer& value(void);
-    // 设置mark标志位
-    void set_mark(EKeyValueMarkType type, bool value, EValueLengthExtendType value_len_extend_type = EValueLengthExtendType_1bit);
+
+    // 设置mark_中某段位置的比特位
+    inline void set_bit(uint8_t pos, uint8_t value, uint8_t bit_len);
+    // 获取mark_某段位置的比特位
+    inline uint8_t get_bit(uint8_t pos, uint8_t bit_len);
     
+    // 设置校验
+    void check_crc16(void) {}
+    // 设置扩展内容
+    void set_extend_data(basic::ByteBuffer extends_data) {}
+    // 获取扩展内容
+    basic::ByteBuffer& get_extend_data(void);
+
     // 重置键值对
     void reset(void);
 
     // 生成一个键值数据块
-    basic::ByteBuffer to_block(void);
+    basic::ByteBuffer to_stream(void);
     // 从键值数据块获取数据
-    bool from_block(const basic::ByteBuffer &block);
-
-private:
-    void set_bit(uint8_t pos, uint8_t value, uint8_t bit_len);
-    uint8_t get_bit(uint8_t pos, uint8_t bit_len);
+    bool from_stream(const basic::ByteBuffer &block);
 
 private:
     uint8_t mark_;
+    uint16_t crc16_;
 
     std::string key_;
     basic::ByteBuffer value_;
 
-    // 错误处理
-    basic::ErrHandle err_handle_;
+    basic::ByteBuffer extend_data_;
 };
 #endif
